0f3621fdb9450059467a7a1983eaa281943b7cdb
iSharpeners <raman@isharpeners.com>
2020-08-23 15:15:35 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_sqlite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_sqlite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Infinite Scrolling',
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
+  }
+}
